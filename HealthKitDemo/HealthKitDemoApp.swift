5cca68bbe907508d09bc15b535d581954809b9d3
richa0604 <ri0601cha@gmail.com>
2026-06-15 23:26:23 +0530

    Initial Commit

---
+++ HealthKitDemo/HealthKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitDemoApp.swift
+//  HealthKitDemo
+//
+//  Created by Richa Rich on 15/06/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
